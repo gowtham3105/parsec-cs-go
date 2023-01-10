374470b66d392c094c46e5ff0d137658df9feefb
Gowtham Sai <66207607+gowtham3105@users.noreply.github.com>
2023-01-10 13:09:29 +0530

    Writing Stats to log file.

--- models/Point.py
+++ models/Point.py
@@ -1,12 +1,28 @@
+from utils import unformat
+
+
 class Point:
     """A model of a 2-d cartesian coordinate Point."""
     x: float
     y: float
+    STRING: str = "Point({x}, {y})"
 
     def __init__(self, x: float, y: float):
         """Construct a point with x, y coordinates."""
         self.x = x
         self.y = y
+
+    def __init__(self, string: str):
+        """Construct a point from a string."""
+        string = unformat(string, Point.STRING)
+        if "x" in string:
+            self.x = string["x"]
+        else:
+            raise ValueError("Unable to Find x coordinate")
+        if "y" in string:
+            self.y = string["y"]
+        else:
+            raise ValueError("Unable to Find y coordinate")
 
     def add(self, other):
         """Add two Point objects together and return a new Point."""
@@ -19,3 +35,11 @@ class Point:
         x: float = self.x - other.x
         y: float = self.y - other.y
         return (x ** 2 + y ** 2) ** 0.5
+
+    def __str__(self) -> str:
+        """Return a string representation of the point."""
+        return Point.STRING.format(self.x, self.y)
+
+    def __repr__(self) -> str:
+        """Return a string representation of the point."""
+        return Point.STRING.format(self.x, self.y)
